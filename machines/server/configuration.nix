1c803ae9973aca5df263fd4502b7315d220c9b71
Martin Elias <melias122@gmail.com>
2021-09-23 16:02:30 +0200

    fix server

--- machines/server/configuration.nix
+++ machines/server/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
 
-      ../roles/common.nix
+      ../../roles/common.nix
     ];
 
   # Use the GRUB 2 boot loader.
