d8643bf9c4be6ee675f0aa8b02af86e549c9651f
Martin Elias <melias122@gmail.com>
2022-04-07 19:54:00 +0200

    update dyndns to digitalocean

--- machines/router/cron.nix
+++ machines/router/cron.nix
@@ -7,7 +7,7 @@ in {
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/1 * * * *      root    curl -s https://api.myip.com | jq -j .ip > /tmp/ipaddr; VULTR_API_KEY=${secrets.vultr-api-key} vultr-cli dns record update ${secrets.domain} -d $(cat /tmp/ipaddr)"
+      "*/1 * * * *      root    /root/go/bin/dnsctl -token=${secrets.digitalocean-token} -no6 -hostname=${secrets.domain}"
     ];
   };
 }
