a53b263ff9ac9a40d649ed7355d080f6f387e287
Martin Elias <melias122@gmail.com>
2022-11-27 22:29:57 +0100

    update cron

--- machines/router/cron.nix
+++ machines/router/cron.nix
@@ -7,7 +7,7 @@ in {
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/1 * * * *      root    /root/go/bin/dnsctl -token=${secrets.digitalocean-token} -no6 -hostname=${secrets.domain}"
+      "*/1 * * * *      root    cd /root/dnsctl && go run main.go -token=${secrets.digitalocean-token} -no6 -hostname=${secrets.domain}"
     ];
   };
 }
