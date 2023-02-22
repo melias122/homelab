f0411c3f03c9c22c1ca92cd704b154c0e2662633
Martin Eliáš <martin@elias.sx>
2023-02-22 09:37:14 +0100

    nixos-hardware: use t14 hardware quirks

--- flake.nix
+++ flake.nix
@@ -5,13 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # NixOS profiles to optimize settings for different hardware.
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-hardware, ... }: {
     nixosConfigurations = {
       t14 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
           ./machines/t14
           home-manager.nixosModules.home-manager
           {
