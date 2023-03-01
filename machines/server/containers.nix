de1f39050a6398638023981e50e9e9ea6330890a
Martin Eliáš <martin@elias.sx>
2023-03-01 16:30:46 +0100

    server: workaroud for oscam manual restarts

--- machines/server/containers.nix
+++ machines/server/containers.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  # This is a workaroud for oscam, which from time to time stops working.
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "*/5 * * * *     root     nc -zvw10 100.98.141.25 8888 &>/dev/null || systemctl restart podman-oscam.service"
+    ];
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
