1cb29f96526e9a072273a308ad90ecf09602824f
Martin Eliáš <martin@elias.sx>
2023-07-14 08:46:04 +0200

    limit boot entries to avoid getting full /boot/efi

--- machines/t14/configuration.nix
+++ machines/t14/configuration.nix
@@ -23,6 +23,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 15;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.kernel.sysctl = {
