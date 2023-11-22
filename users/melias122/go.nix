4676aeef18c2a3e7066cf31525ed1ff789d1ad1e
Martin Eliáš <martin@elias.sx>
2023-11-22 21:36:15 +0100

    go

--- users/melias122/go.nix
+++ users/melias122/go.nix
@@ -3,15 +3,13 @@
 {
   home.packages = with pkgs; [
     delve
-    golangci-lint
-    golint
     gopls
   ];
 
   # add CGO_ENABLED=0
   programs.go = {
     enable = true;
-    package = pkgs.go_1_20;
+    package = pkgs.go_1_21;
   };
 
   home.sessionPath = [
