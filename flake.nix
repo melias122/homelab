7eca7b838a43e749bf0a0e926385d83131b351d4
Martin Eliáš <martin@elias.sx>
2024-09-30 08:39:45 +0200

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
--- users/melias122/go.nix
+++ users/melias122/go.nix
@@ -1,18 +1,29 @@
 { config, pkgs, ... }:
 
-{
+let
+  pprofbench = pkgs.writeScriptBin "pprofbench"
+    ''
+			#!/bin/bash
+			set -ex
+			go test -bench=''${1:-.} -cpuprofile=cpu.pprof -memprofile=mem.pprof -count=5
+		'';
+
+  scripts = [
+    pprofbench
+  ];
+
+in {
   home.packages = with pkgs; [
     delve
     gopls
-  ];
+  ] ++ scripts;
 
-  # add CGO_ENABLED=0
   programs.go = {
     enable = true;
-    package = pkgs.go_1_22;
   };
 
   home.sessionPath = [
+    "$HOME/code/go/bin" # For custom go version's
     "$HOME/go/bin"
   ];
 }
