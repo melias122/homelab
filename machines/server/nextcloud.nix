96c9c03029d2282c84239c64898786b0bf3e2671
Martin Eliáš <martin@elias.sx>
2024-10-15 09:44:22 +0200

    Update nextcloud

--- machines/server/nextcloud.nix
+++ machines/server/nextcloud.nix
@@ -3,54 +3,23 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud31;
     hostName = "nextcloud.elias.sx";
-    home = "/pool/nextcloud";
+    home = "/pool/nextcloud-new";
     https = true;
     autoUpdateApps.enable = true;
-    autoUpdateApps.startAt = "05:00:00";
     maxUploadSize = "16G";
     configureRedis = true;
     config = {
       adminuser = "melias122";
       adminpassFile = "/etc/nixos/x/nextcloud-adminpass";
-      dbtype = "pgsql";
-      dbuser = "nextcloud";
-      dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-      dbname = "nextcloud";
+      dbtype = "sqlite";
     };
-  };
-
-  # ensure that postgres is running *before* running the setup
-  systemd.services."nextcloud-setup" = {
-    requires = ["postgresql.service"];
-    after = ["postgresql.service"];
   };
 
   services.nginx.virtualHosts = {
     "nextcloud.elias.sx" = {
       listen = [{ addr = "100.98.141.25"; port = 54443; }]; # use tailscale interface
     };
-  };
-
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_15;
-    dataDir = "/nvme/postgres";
-    ensureDatabases = [ "nextcloud" ];
-    ensureUsers = [
-      {
-        name = "nextcloud";
-        ensurePermissions = {
-          "DATABASE nextcloud" = "ALL PRIVILEGES"; # GRANT ALL ON SCHEMA public TO nextcloud
-        };
-      }
-    ];
-  };
-
-  services.postgresqlBackup = {
-    enable = true;
-    location = "/pool/postgres-backup";
-    databases = [ "nextcloud" ];
   };
 }
