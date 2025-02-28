20cf444dc2f1cc369111b02cb9557cf05f29267d
Martin Eliáš <martin@elias.sx>
2025-02-28 21:46:14 +0100

    Update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # NixOS profiles to optimize settings for different hardware.
