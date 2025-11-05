f8ecb0655f5ccca571d8b37eb87fac6595bab3e2
Martin Eliáš <martin@elias.sx>
2025-11-05 16:48:52 +0100

    Delete died t14 migrate to per machine flake

--- machines/box/flake.nix
+++ machines/box/flake.nix
@@ -1,14 +1,10 @@
 {
-  description = "NixOS configuration";
+  description = "NixOS configuration for box machine";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # NixOS profiles to optimize settings for different hardware.
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, nixos-hardware, ... }: let
@@ -20,35 +16,21 @@
       melias122 = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
-          ./users/melias122
+          ../../users/melias122
         ];
       };
     };
 
-    # pure NixOS config
     nixosConfigurations = {
-      t14 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen1
-          ./machines/t14
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.melias122 = import ./users/melias122;
-          }
-        ];
-      };
       box = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./machines/box
+          ./.
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.melias122 = import ./users/melias122;
+            home-manager.users.melias122 = import ../../users/melias122;
           }
         ];
       };
--- machines/t14/default.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  imports = [ ./configuration.nix ];
-}
