613afeb2e352d1c2e666125ab694f6650c26734c
Martin Eliáš <martin@elias.sx>
2026-07-31 13:19:55 +0200

    services: update config

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    tailscale
-  ];
-
-  services.tailscale = {
-    enable = true;
-  };
+  services.tailscale.enable = true;
 }
